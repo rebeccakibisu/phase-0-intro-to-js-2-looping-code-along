0c92d6b6e3e39e6bb1e4b04acafc5102e34c9c80
Rebecca Kibisu <coursecpd@gmail.com>
2025-07-04 06:28:41 +0300

    loop

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+  let messages = [];
+
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+
+  return messages;
+}
+
+// Insert names in the array
+const namesArray = ["Guadalupe", "Ollie", "Aki"];
+const eventName = "surprise";
+
+// Call the function
+const thankYouMessages = writeCards(namesArray, eventName);
+
+// Log the result
+console.log(thankYouMessages);
+
+function countDown(number) {
+  for (let i = number; i >= 0; i--) {
+    console.log(i);
+  }
+}
